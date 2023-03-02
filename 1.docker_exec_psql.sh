65eaa8a3d280574c1a2aff79a61cf63740b70116
Paulo AC <zipizap123@gmail.com>
2023-03-03 00:33:11 +0100

    small improv

--- 1.docker_exec_psql.sh
+++ 1.docker_exec_psql.sh
@@ -3,7 +3,7 @@ set -xefu
 
 # The localhost connection from inside same container is trusted, does not require password.
 # But remote connections require password which can be set with PGPASSWORD=mysecretpassword
-docker exec -it mypostgres psql -U postgres -d postgres
+docker exec -it mypostgres psql -h 127.0.0.1 -U postgres -d postgres
 
 #docker exec -it mypostgres bash
 
